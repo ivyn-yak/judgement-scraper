334f1e4cb479b33aac0fc98606d497e5b352fd77
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-10-17 19:00:44 +0800

    download pdfs into folders

--- scraper.py
+++ scraper.py
@@ -1,19 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from utils import *
 
 driver = webdriver.Chrome()
 
 driver.get("https://www.singaporelawwatch.sg/Judgments")
 
 title = driver.title
-
 driver.implicitly_wait(10)  
 
-judgement_types = driver.find_elements(By.CSS_SELECTOR, "div.news.NewsMagazine_Style_custom_st5dovjofqg.eds_style_custom_st5dovjofqg.eds_subCollection_latestArticles.eds_subCollection_news.eds_news_NewsMagazine.eds_template_List_Article_Simple-SLW_Judgments")
+judgements = driver.find_element(By.ID, "dnn_leftPane8")
+judgement_types = judgements.find_elements(By.CSS_SELECTOR, "div.eds_containers_NewsMagazine.eds_templateGroup_default.eds_template_Default.eds_style_custom_sw3m9ntp2vf")
 for type in judgement_types:
+    h2 = type.find_element(By.CSS_SELECTOR, "h2")
+    folder_name = "Judgements/" + h2.text
+    create_directory(folder_name)
+
     articles = type.find_elements(By.CSS_SELECTOR, "div.edn__articleListWrapper article h4 a")
     for a in articles:
         href = a.get_attribute('href')  
         article_text = a.text
-        print(">>>>>>", a.text, "Link: ", href)
+        file_name = article_text.replace(" ", "_")
+        pdf = file_name + ".pdf"
+
+        download_file(href, folder_name, pdf)
+
 driver.quit()
---
+++ utils.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import os
+import requests
+
+def create_directory(dir_path):
+    Path(dir_path).mkdir(parents=True, exist_ok=True)
+
+def download_file(url, folder_name, file_name):
+    file_path = os.path.join(folder_name, file_name)
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+            print(f"Downloaded: {file_name} to {folder_name}")
+    else:
+        print(f"Failed to download: {file_name} from {url}")
