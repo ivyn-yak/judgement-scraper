5b7fc1dc6113ea99e1be67bd49eed5880c186a11
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-10-19 22:51:47 +0800

    handled pagination

--- scraper.py
+++ scraper.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from utils import *
+
+def download_articles(type, folder_name):
+    articles = type.find_elements(By.CSS_SELECTOR, "div.edn__articleListWrapper article h4 a")
+    for a in articles:
+        href = a.get_attribute('href')  
+        article_text = a.text
+        file_name = article_text.replace(" ", "_")
+        pdf = file_name + ".pdf"
+        
+        download_file(href, folder_name, pdf)
+        print(pdf)
 
 driver = webdriver.Chrome()
 
@@ -9,20 +23,45 @@ driver.get("https://www.singaporelawwatch.sg/Judgments")
 title = driver.title
 driver.implicitly_wait(10)  
 
-judgements = driver.find_element(By.ID, "dnn_leftPane8")
-judgement_types = judgements.find_elements(By.CSS_SELECTOR, "div.eds_containers_NewsMagazine.eds_templateGroup_default.eds_template_Default.eds_style_custom_sw3m9ntp2vf")
-for type in judgement_types:
-    h2 = type.find_element(By.CSS_SELECTOR, "h2")
-    folder_name = "Judgements/" + h2.text
+def main(id, folder_name):
+    judgements = driver.find_element(By.ID, "dnn_leftPane8")
+    judgement_type = judgements.find_element(By.CSS_SELECTOR, f"div.DnnModule.DnnModule-EasyDNNnewsWidgets.DnnModule-{id}")
+    download_articles(judgement_type, folder_name)
+
+    while True:
+        try:
+            pagination = WebDriverWait(judgement_type, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.article_pager a.next")))
+            pagination_href = pagination.get_attribute('href')
+            print(f"\nNext page href: {pagination_href}")
+
+            pagination.click() 
+
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "dnn_leftPane8")))
+            judgements = driver.find_element(By.ID, "dnn_leftPane8")
+            judgement_type = judgements.find_element(By.CSS_SELECTOR, f"div.DnnModule.DnnModule-EasyDNNnewsWidgets.DnnModule-{id}")
+            download_articles(judgement_type, folder_name)
+
+        except TimeoutException:
+            print(f"\nNo more pages available for id {id}. Files downloaded in {folder_name}.\n")
+            break
+
+        except NoSuchElementException:
+            print(f"\nPagination element not found for id {id}. It may indicate the end of available pages.\n")
+            break
+
+        except Exception as e:
+            print(f"\nAn error occurred: {str(e)}")
+            break 
+
+ids = ["452", "451", "450", "449"]
+for id in ids:
+    judgements = driver.find_element(By.ID, "dnn_leftPane8")
+    judgement_type = judgements.find_element(By.CSS_SELECTOR, f"div.DnnModule.DnnModule-EasyDNNnewsWidgets.DnnModule-{id}")
+    h2 = judgement_type.find_element(By.CSS_SELECTOR, "h2")
+    folder_name = f"Judgements/{h2.text}"
     create_directory(folder_name)
 
-    articles = type.find_elements(By.CSS_SELECTOR, "div.edn__articleListWrapper article h4 a")
-    for a in articles:
-        href = a.get_attribute('href')  
-        article_text = a.text
-        file_name = article_text.replace(" ", "_")
-        pdf = file_name + ".pdf"
-
-        download_file(href, folder_name, pdf)
+    print(f"\n---- Downloading files with id:{id} into {folder_name} ----")
+    main(id, folder_name)
 
 driver.quit()
